69b8bd5ea641d33de5de5ad30d1672d717d00839
Talhah Zahid <talhazahid415@gmail.com>
2025-11-04 16:13:49 +0500

    integrate cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express, { urlencoded } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 import connectdb from "./src/database/db.js";
@@ -7,16 +8,16 @@ import router from "./src/routes/user_message.routes.js";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:5173",
-//       "https://microfinance-app-frontend-phi.vercel.app",
-//       "https://microfinance-app-dashboard.vercel.app",
-//     ],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      // "https://microfinance-app-frontend-phi.vercel.app",
+      // "https://microfinance-app-dashboard.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(urlencoded({ extended: false }));
 app.get("/", (req, res) => {
